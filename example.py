dedea9ea7fabd32f73042fff472830422e67828e
simulationguest <leo@lelux.net>
2022-06-23 14:27:54 +0200

    wer braucht error handling wenn man neustarten kann

--- example.py
+++ example.py
@@ -1,17 +1,13 @@
 from mq import *
 import sys, time
 
-try:
-    print("Press CTRL+C to abort.")
-    
-    mq = MQ();
-    while True:
-        perc = mq.MQPercentage()
-        sys.stdout.write("\r")
-        sys.stdout.write("\033[K")
-        sys.stdout.write("%g;", perc["CO"])
-        sys.stdout.flush()
-        time.sleep(0.1)
+print("Press CTRL+C to abort.")
 
-except:
-    print("\nAbort by user")
+mq = MQ();
+while True:
+    perc = mq.MQPercentage()
+    sys.stdout.write("\r")
+    sys.stdout.write("\033[K")
+    sys.stdout.write("%g;", perc["CO"])
+    sys.stdout.flush()
+    time.sleep(0.1)
