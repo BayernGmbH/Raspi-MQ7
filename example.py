893c5e4a76cb6a9aa05d7d7e1c4a79d08b4de2a6
simulationguest <leo@lelux.net>
2022-06-23 14:39:17 +0200

    deine mom ist fett

--- example.py
+++ example.py
@@ -8,6 +8,6 @@ while True:
     perc = mq.MQPercentage()
     sys.stdout.write("\r")
     sys.stdout.write("\033[K")
-    sys.stdout.print("%g," % perc["CO"])
+    sys.stdout.write("%g," % perc["CO"])
     sys.stdout.flush()
     time.sleep(0.1)
