28476a2959e11f9dbeb99493d1f04fbc3e08cbe4
simulationguest <leo@lelux.net>
2022-06-23 14:36:18 +0200

    leo is zu dumm zum abschreiben

--- example.py
+++ example.py
@@ -8,6 +8,6 @@ while True:
     perc = mq.MQPercentage()
     sys.stdout.write("\r")
     sys.stdout.write("\033[K")
-    sys.stdout.write("%g;", perc["CO"])
+    sys.stdout.print("%g," % perc["CO"])
     sys.stdout.flush()
     time.sleep(0.1)
